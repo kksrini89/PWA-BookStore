6a8d28b44b6cbf449c516ee554024b339963f709
kksrinivasan <kksrini89@gmail.com>
2019-11-26 09:00:29 +0530

    header designed

---
+++ src/app/data/books.ts
@@ -0,0 +1,29 @@
+interface book {}
+
+export const books = [
+  {
+    title: "",
+    short_note: "",
+    cover_image: "",
+    is_foreworded: "",
+    foreworded_authors: [
+      {
+        name: "",
+        wiki_bio_link: ""
+      },
+      {
+        name: "",
+        wiki_bio_link: ""
+      }
+    ],
+    reviews: {
+      rating: "",
+      comment: "",
+      commented_date: ""
+    },
+    published_date: "",
+    author: "",
+    category: "",
+    price: ""
+  }
+];
