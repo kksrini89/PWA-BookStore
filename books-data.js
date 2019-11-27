0cf24a78e3eaf5feb8b1c8dc37ff9ffb492eec97
kksrinivasan <kksrini89@gmail.com>
2019-11-27 22:56:01 +0530

    Data structure is defined & styles are added

---
+++ books-data.js
@@ -0,0 +1,128 @@
+const books = [
+  {
+    title: "So good they can't ignore you",
+    short_note:
+      "In So Good They Can't Ignore You, Cal Newport sets out on a quest to discover the reality of how people end up loving what they do. Spending time with organic farmers, venture capitalists, screenwriters, freelance computer programmers and others who admitted to deriving great satisfaction from their work, Newport debunks the long-held belief that 'following your passion' is the key to satisfaction.",
+    cover_image: "https://via.placeholder.com/150",
+    is_foreworded: false,
+    foreworded_authors: [
+      {
+        name: "",
+        wiki_bio_link: ""
+      },
+      {
+        name: "",
+        wiki_bio_link: ""
+      }
+    ],
+    reviews: {
+      rating: "",
+      comment: "",
+      commented_date: ""
+    },
+    language: "English",
+    published_date: "31 Jan 2018",
+    author: "Cal Newport",
+    category: "self-help",
+    money_symbol: "₹",
+    price: 310
+  },
+  {
+    title: "Awaken the Giant within",
+    short_note:
+      "The author of 'Unlimited Power' now offers advice on such matters as mastering emotions, overcoming debilitating habits such as over-eating, drinking and drug abuse, unleashing the hidden power of body and mind, improving personal and professional relationships, and taking control of personal finances.",
+    cover_image: "https://via.placeholder.com/150",
+    is_foreworded: false,
+    foreworded_authors: [
+      {
+        name: "",
+        wiki_bio_link: ""
+      },
+      {
+        name: "",
+        wiki_bio_link: ""
+      }
+    ],
+    reviews: [
+      {
+        rating: "4",
+        comment:
+          "yes this is a great book for anyone who is seriously intrested in bringing about big changes in oneself",
+        commented_date: "2014-08-14"
+      }
+    ],
+    language: "English",
+    published_date: "1 Jan 1992",
+    author: "Anthony Robbins",
+    category: "self-help",
+    money_symbol: "₹",
+    price: 373
+  },
+  {
+    title: "The Magic of thinking Big",
+    short_note:
+      "David J Schwartz's classic teachings are as powerful today as they were then. Practical, empowering and hugely engaging, this book will not only inspire you, it will give you the tools to change your life for the better - starting from now.",
+    cover_image: "https://via.placeholder.com/150",
+    is_foreworded: false,
+    foreworded_authors: [
+      {
+        name: "",
+        wiki_bio_link: ""
+      },
+      {
+        name: "",
+        wiki_bio_link: ""
+      }
+    ],
+    reviews: [
+      {
+        rating: "5",
+        comment:
+          "Books on personal development, or the so-called ‘self’-help genre, have been around for so long, there is no doubt about that. There is a plethora of such books, which promise to help a person overcome the tough situations in life and sail through smoothly",
+        commented_date: "15 October 2017"
+      }
+    ],
+    language: "English",
+    published_date: "29 Feb 2016",
+    author: "David J Schwartz",
+    category: "self-help",
+    money_symbol: "₹",
+    price: 278
+  },
+  {
+    title: "the POWER",
+    short_note:
+      "The power book is a motivational book to inspire people and make them realise that real power lies within themselves.",
+    cover_image: "https://via.placeholder.com/150",
+    is_foreworded: false,
+    foreworded_authors: [
+      {
+        name: "",
+        wiki_bio_link: ""
+      },
+      {
+        name: "",
+        wiki_bio_link: ""
+      }
+    ],
+    reviews: [
+      {
+        rating: "5",
+        comment: "A good book. A boom worth adding to your collections.",
+        commented_date: "11 August 2019"
+      },
+      {
+        rating: "1",
+        comment:
+          "Book quality isn't good enough. Pages are rough, print is uneven. Seems like a pirated version of original book.",
+        commented_date: "13 April 2019"
+      }
+    ],
+    language: "English",
+    published_date: "2015",
+    author: "Rhonda Byrne",
+    category: "self-help",
+    money_symbol: "₹",
+    price: 210
+  }
+];
