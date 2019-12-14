0b7135eb8fb35cc1ec6c678cd08791ed63757eaf
Srinivasan K K <srinivasan.k.k@kdi.kongsberg.com>
2019-12-14 17:09:31 +0530

    Both UI for filter, book region has been updated

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
+// import data from './books-data';
+
 if (navigator.serviceWorker) {
   navigator.serviceWorker
-    .register('./sw.js')
-    .then(() => console.log('[SW] is registered.'))
+    .register("./sw.js")
+    .then(() => console.log("[SW] is registered."))
     .catch(console.error);
 }
+
+const applied_filter_tmpl = `<div>
+<span class="close">X</span>
+<span class="filter-text"></span>
+</div>`;
+
+const book = {
+  populate() {
+    console.log(books);
+    const containerElement = document.querySelector(".books-area");
+    books.forEach(book => {
+      const book_info_tmpl = `<figure class="image book-figure">
+        <image class="book-image" src="https://bulma.io/images/placeholders/256x256.png"></image>
+      </figure>
+      <div class="book-meta">
+        <div class="book-name">${book.title}</div>
+        <div class="book-author">${book.author}</div>
+      </div>`;
+      const bookInfoElement = document.createElement("div");
+      bookInfoElement.classList.add("book-info");
+      bookInfoElement.innerHTML = book_info_tmpl;
+      containerElement.append(bookInfoElement);
+    });
+  }
+};
+
+book.populate();
