62061c8b36bcbea1572d4eed2db5e6bfe58ab4c7
Srinivasan K K <srinivasan.k.k@kdi.kongsberg.com>
2019-12-16 23:13:17 +0530

    Display book UI is designed

--- main.js
+++ main.js
@@ -13,22 +13,33 @@ const applied_filter_tmpl = `<div>
 </div>`;
 
 const book = {
-  populate() {
-    console.log(books);
+  async populate() {
     const containerElement = document.querySelector(".books-area");
-    books.forEach(book => {
+
+    const isbns = await Promise.all(
+      isbn_list.map(book =>
+        this.getBookCover(book).then(({ items }) => items[0].volumeInfo)
+      )
+    );
+    console.log(isbns);
+    isbns.forEach(book => {
       const book_info_tmpl = `<figure class="image book-figure">
-        <image class="book-image" src="https://bulma.io/images/placeholders/256x256.png"></image>
+        <image class="book-image" src=${book.imageLinks.thumbnail}></image>
       </figure>
       <div class="book-meta">
-        <div class="book-name">${book.title}</div>
-        <div class="book-author">${book.author}</div>
+        <div class="book-name" title=${book.title}>${book.title}</div>
+        <div class="book-author" title=${book.authors.join(',')}>${book.authors.join(',')}</div>
       </div>`;
       const bookInfoElement = document.createElement("div");
       bookInfoElement.classList.add("book-info");
       bookInfoElement.innerHTML = book_info_tmpl;
       containerElement.append(bookInfoElement);
     });
+  },
+  getBookCover(isbn) {
+    return fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`, {
+      method: "GET"
+    }).then(d => d.json());
   }
 };
 
